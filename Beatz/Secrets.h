993a8bfe55673c202ad66b1048a35f06745b6238
aabato <angelica.bato@gmail.com>
2016-05-18 16:53:16 -0400

    Secrets, I want to know secrets. What's your dad's name

---
+++ Beatz/Secrets.h
@@ -0,0 +1,13 @@
+//
+//  Secrets.h
+//  Beatz
+//
+//  Created by Angelica Bato on 5/18/16.
+//  Copyright © 2016 Angelica Bato. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const SpotifyClientID;
+extern NSString * const SpotifyClientSecret;
+
