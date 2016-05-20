60291dcdd715a6eee7720e2b2ffe19ad16767dec
aabato <angelica.bato@gmail.com>
2016-05-19 20:48:25 -0400

    Add stackview for button and add fxnality to stop recording

--- Beatz/ViewController.m
+++ Beatz/ViewController.m
@@ -13,6 +13,7 @@
 @interface ViewController () <AVCaptureVideoDataOutputSampleBufferDelegate>
 
 @property (strong, nonatomic) UIButton *startPulseButton;
+@property (strong, nonatomic) UIButton *stopPulseButton;
 @property (strong, nonatomic) AVCaptureSession *session;
 @property (strong, nonatomic) AVCaptureDevice *camera;
 
@@ -35,6 +36,12 @@
     
     [self startCameraCapture];
     
+}
+
+- (IBAction)stopPulseButtonTapped:(id)sender {
+    NSLog(@"Stopped!");
+    [self.session stopRunning];
+    self.session = nil;
 }
 
 - (void)startCameraCapture {
@@ -65,22 +72,47 @@
 }
 
 - (void) buildInitialView {
+    
+    //Gradient
+    self.view.tintColor = [UIColor colorWithRed:184.0/255.0 green:26.0/255.0 blue:67.0/255.0 alpha:1.0];
+    UIColor *gradientMaskLayer = [UIColor colorWithRed:131.0/255.0 green:26.0/255.0 blue:36.0/255.0 alpha:1.0];
+    CAGradientLayer *gradientMask = [CAGradientLayer layer];
+    gradientMask.frame = self.view.bounds;
+    gradientMask.colors = @[(id)gradientMaskLayer.CGColor,(id)[UIColor clearColor].CGColor];
+    
+    [self.view.layer insertSublayer:gradientMask atIndex:0];
+    
     self.startPulseButton = [[UIButton alloc] init];
-    [self.startPulseButton setTitle:@"Collect heartbeat" forState:UIControlStateNormal];
+    [self.startPulseButton setTitle:@"Collect pulse" forState:UIControlStateNormal];
     [self.startPulseButton setBackgroundColor:[UIColor lightGrayColor]];
     [self.startPulseButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     self.startPulseButton.layer.cornerRadius = 20;
     self.startPulseButton.clipsToBounds = YES;
     [self.startPulseButton addTarget:self action:@selector(startPulseButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
     
-    [self.view addSubview:self.startPulseButton];
+    self.stopPulseButton = [[UIButton alloc] init];
+    [self.stopPulseButton setTitle:@"Stop collecting pulse" forState:UIControlStateNormal];
+    [self.stopPulseButton setBackgroundColor:[UIColor lightGrayColor]];
+    [self.stopPulseButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    self.stopPulseButton.layer.cornerRadius = 20;
+    self.stopPulseButton.clipsToBounds = YES;
+    [self.stopPulseButton addTarget:self action:@selector(stopPulseButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIStackView *myStack = [[UIStackView alloc] initWithArrangedSubviews:@[self.startPulseButton, self.stopPulseButton]];
+    myStack.axis = UILayoutConstraintAxisVertical;
+    
+    [self.view addSubview:myStack];
     
     self.view.translatesAutoresizingMaskIntoConstraints = NO;
-    self.startPulseButton.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.startPulseButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
-    [self.startPulseButton.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor].active = YES;
-    [self.startPulseButton.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.7].active = YES;
-    [self.startPulseButton.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:0.1].active = YES;
+    myStack.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [myStack.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+    [myStack.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor].active = YES;
+    [myStack.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.7].active = YES;
+    [myStack.heightAnchor constraintEqualToAnchor:self.view.heightAnchor multiplier:0.3].active = YES;
+    
+    myStack.distribution = UIStackViewDistributionFillEqually;
+    myStack.spacing = 15.0;
 }
 
 @end
