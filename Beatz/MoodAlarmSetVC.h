d0b977aea439a5d4575690dc480b20b89287fd9a
aabato <angelica.bato@gmail.com>
2016-05-23 17:46:26 -0400

    Pivot project

---
+++ Beatz/MoodAlarmSetVC.h
@@ -0,0 +1,13 @@
+//
+//  MoodAlarmSetVC.h
+//  Beatz
+//
+//  Created by Angelica Bato on 5/23/16.
+//  Copyright © 2016 Angelica Bato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoodAlarmSetVC : UIViewController
+
+@end
